3ae9bbade2c2739c7e9bc64f8bd2529ca04ff0c5
Niklas P <contact@niklasplessing.net>
2025-09-29 12:05:01 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Status } from "./status";
+import { ConnectWallet } from "./components/connect-wallet.dedot";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Poladot-UI Component Test</h1>
-      {/* Put Component Here */}
+      <ConnectWallet />
       <Status />
     </div>
   );
