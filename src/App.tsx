97f986d5036a423ead9f9baa68f4c7888914260f
Niklas P <contact@niklasplessing.net>
2025-09-29 12:40:29 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import { AccountInfo } from "./components/account-info.dedot";
 import { Status } from "./status";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Poladot-UI Component Test</h1>
-      {/* Put Component Here */}
+      <AccountInfo address="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY" />
       <Status />
     </div>
   );
