0258c1c57ebd073242b886404ae95ffc02200f50
Niklas P <contact@niklasplessing.net>
2025-09-30 20:45:39 +0200

    add paseo

--- src/lib/reactive-dot.config.ts
+++ src/lib/reactive-dot.config.ts
@@ -1,4 +1,7 @@
 import {
+  paseo,
+  paseo_asset_hub,
+  paseo_people,
   polkadot_asset_hub,
   polkadot_people,
   polkadot,
@@ -11,8 +14,17 @@ import {
   type WsJsonRpcProvider,
 } from "polkadot-api/ws-provider";
 
+let paseoPeopleProvider: WsJsonRpcProvider | null = getWsProvider(
+  "wss://sys.ibp.network/people-paseo"
+);
 let polkadotPeopleProvider: WsJsonRpcProvider | null = getWsProvider(
   "wss://sys.ibp.network/people-polkadot"
+);
+let paseoProvider: WsJsonRpcProvider | null = getWsProvider(
+  "wss://sys.ibp.network/paseo"
+);
+let paseoAssetHubProvider: WsJsonRpcProvider | null = getWsProvider(
+  "wss://sys.ibp.network/asset-hub-paseo"
 );
 let polkadotAssetHubProvider: WsJsonRpcProvider | null = getWsProvider(
   "wss://sys.ibp.network/asset-hub-polkadot"
@@ -22,7 +34,10 @@ let polkadotProvider: WsJsonRpcProvider | null = getWsProvider(
 );
 
 export const destroyProviders = () => {
+  paseoPeopleProvider = null;
   polkadotPeopleProvider = null;
+  paseoProvider = null;
+  paseoAssetHubProvider = null;
   polkadotAssetHubProvider = null;
   polkadotProvider = null;
   polkadotAssetHubProvider = null;
@@ -31,6 +46,33 @@ export const destroyProviders = () => {
 export const config = defineConfig({
   ssr: true,
   chains: {
+    paseo: {
+      name: "Paseo",
+      descriptor: paseo,
+      provider: paseoProvider,
+      explorerUrl: "https://paseo.subscan.io",
+      symbol: "PAS",
+      decimals: 10,
+      logo: "https://raw.githubusercontent.com/Koniverse/SubWallet-ChainList/refs/heads/master/packages/chain-list-assets/public/assets/chain-assets/paseotest-native-pas.png",
+    },
+    paseoPeople: {
+      name: "Paseo People",
+      descriptor: paseo_people,
+      provider: paseoPeopleProvider,
+      explorerUrl: "https://people-paseo.subscan.io",
+      symbol: "PAS",
+      decimals: 10,
+      logo: "https://people-paseo.subscan.io/_next/image?url=%2Fchains%2Fpeople-paseo%2Flogo-mini.png&w=256&q=75",
+    },
+    paseoAssetHub: {
+      name: "Paseo Asset Hub",
+      descriptor: paseo_asset_hub,
+      provider: paseoAssetHubProvider,
+      explorerUrl: "https://assethub-paseo.subscan.io",
+      symbol: "PAS",
+      decimals: 10,
+      logo: "https://raw.githubusercontent.com/Koniverse/SubWallet-ChainList/refs/heads/master/packages/chain-list-assets/public/assets/chains/paseo_assethub.png",
+    },
     polkadot: {
       name: "Polkadot",
       descriptor: polkadot,
