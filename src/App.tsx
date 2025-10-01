1df340835795f0a33dd61a962a3d1e313480d1f1
Niklas P <contact@niklasplessing.net>
2025-10-01 17:30:30 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Status } from "./status";
 import { PolkadotProvider } from "./lib/polkadot-provider.papi";
+import { AccountInfo } from "./components/account-info.papi";
 
 function App() {
   return (
     <PolkadotProvider>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1>Poladot-UI Component Test</h1>
-        {/* Put Component Here */}
+        <AccountInfo address="5C74C7pzE1pYVuK8F9q3xgn7t58Q6N6k9BfFhG9L59y37" />
         <Status />
       </div>
     </PolkadotProvider>
